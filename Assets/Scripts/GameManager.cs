41b9ddee7268dee024507ffaf21ef76a7c12d100
Þorri Bjarnason <123247777+GiantMushy@users.noreply.github.com>
2026-02-03 00:00:15 +0000

    State Switching core functionality achieved

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,89 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Dev Values")]
+    public bool developmentMode = true;
+    [SerializeField] private GameState startingState = GameState.LoadingScreen;
+    private enum GameState { LoadingScreen, LocalVsOnline, StartLocalGame, HostVsJoin }
+    private Dictionary<GameState, GameObject> stateDictionary;
+
+    [Header("State References")]
+    public GameObject loadingScreen;
+    public GameObject localVsOnline;
+    public GameObject startLocalGame;
+    public GameObject hostVsJoin;
+
     void Start()
     {
+        // Initialize the dictionary
+        stateDictionary = new Dictionary<GameState, GameObject>
+        {
+            { GameState.LoadingScreen, loadingScreen },
+            { GameState.LocalVsOnline, localVsOnline },
+            { GameState.StartLocalGame, startLocalGame },
+            { GameState.HostVsJoin, hostVsJoin }
+        };
         
+        if (developmentMode)
+        {
+            Debug.Log("Development Mode: ON");
+            SetState(startingState);
+        }
+        else
+        {
+            Debug.Log("Development Mode: OFF");
+            SetState(GameState.LoadingScreen);
+        }
+    } 
+
+    public void LocalButton()
+    {
+        Debug.Log("Local Button Pressed");
+        SetState(GameState.StartLocalGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnlineButton()
     {
-        
+        Debug.Log("Online Button Pressed");
+        SetState(GameState.HostVsJoin);
+    }
+
+    public void MenuButton()
+    {
+        Debug.Log("Open Menu Button Pressed");
+        SetState(GameState.LocalVsOnline);
+    }
+
+    public void HostButton()
+    {
+        Debug.Log("Host Button Pressed");
+    }
+
+    public void JoinButton()
+    {
+        Debug.Log("Join Button Pressed");
+    }
+
+    private void SetState(GameState newState)
+    {
+        // Disable all states
+        foreach (var state in stateDictionary.Values)
+        {
+            state.SetActive(false);
+        }
+
+        // Enable the desired state
+        if (stateDictionary.ContainsKey(newState))
+        {
+            stateDictionary[newState].SetActive(true);
+            Debug.Log($"Switched to state: {newState}");
+        }
+        else
+        {
+            Debug.LogError($"State {newState} not found in the dictionary!");
+        }
     }
 }
