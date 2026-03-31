9a0b90a5d43cdd38126bc602d6969b33d4e077a5
Þorri Bjarnason <thorvardur23@ru.is>
2026-03-31 12:39:25 +0000

    Language Buttons init Localization

--- Assets/Scripts/PageControllers/SettingsController.cs
+++ Assets/Scripts/PageControllers/SettingsController.cs
@@ -1,5 +1,8 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+    
 public class SettingsController : MonoBehaviour
 {
     [Header("References")]
@@ -13,14 +16,26 @@ public class SettingsController : MonoBehaviour
     {
         gameManager.BackToSavedState();
     }
+    private IEnumerator SetLocale(string code)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+
+        Locale locale = LocalizationSettings.AvailableLocales.GetLocale(code);
+        if (locale != null)
+            LocalizationSettings.SelectedLocale = locale;
+        else
+            Debug.LogWarning("Locale not found: " + code);
+    }
 
     public void Icelandic()
     {
         gameManager.SetLanguage("is");
+        StartCoroutine(SetLocale("is"));
     }
 
     public void English()
     {
         gameManager.SetLanguage("en");
+        StartCoroutine(SetLocale("en"));
     }
 }
