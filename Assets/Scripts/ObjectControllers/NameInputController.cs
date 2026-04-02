b0b27438a7c53d934af62da55851906eb42994ad
Þorri Bjarnason <thorvardur23@ru.is>
2026-04-02 12:41:26 +0000

    Name Input visual fix

--- Assets/Scripts/ObjectControllers/NameInputController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-// This script is no longer needed.
-// All name input logic has been moved to StartLocalGameController.
-// You can safely remove this component from the GameObject in the Unity Inspector.
-public class NameInputController : MonoBehaviour
-{
-}
